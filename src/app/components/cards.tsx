100a8df69d908c090bb05c71ddd30ca4b34082e2
Gogo200202 <gosheto420@gmail.com>
2025-03-11 10:00:00 +0200

    start making cards

---
+++ src/app/components/cards.tsx
@@ -0,0 +1,13 @@
+export default function Cards() {
+  return (
+    <div>
+      <div className=" text-center text-5xl">Най-добри предложения</div>
+
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 ml-10 md:ml-40 lg:ml-96">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+    </div>
+  );
+}
--- src/app/components/searchbare.tsx
+++ src/app/components/searchbare.tsx
@@ -1,58 +1,85 @@
 "use client";
-import React, { useState } from "react";
+
+import React, { useState, useEffect, ChangeEvent } from "react";
 export default function Searchbare() {
-  const [showMessage, setShowMessage] = useState(false);
+
+  const [input, setInput] = useState('');
+
+function typing(event:any){
+  console.log(event.target.value);
+  setInput(event.target.value)
+}
+
+function searchbareIcon(){
   
+ console.log(input);
+}
+
+  useEffect(() => {
+    const keyDownHandler = (event: KeyboardEvent) => {
+    
+      if (event.key === "Enter") {   
+        console.log(input);
+      }
+    };
+
+    document.addEventListener("keydown", keyDownHandler);
+
+    return () => {
+      document.removeEventListener("keydown", keyDownHandler);
+    };
+  }, [input]);
+
+
   return (
     <div className="h-16 w-full border-x-[3vw] border-(--background)   ">
+     
       <div className="bg-whiteSearchBar h-full w-full rounded-md flex">
         <img
+        onClick={searchbareIcon}
           className="h-16 ml-1  flex-none"
           src="/svg/search.svg"
           alt="search"
         />
         <input
           type="text"
+          onChange={typing }
           className="h-16 w-1/3 md:w-2/3 lg:w-5/6 flex-none text-black focus:outline-none "
           placeholder="Search branch name..."
           required
         />
 
-        <div className="flex-none ">
+        <div className="flex-none">
           <div className="group">
-            <img
-              className=" h-16 "
-              src="/svg/filter.svg"
-              alt="filter"
-            />
+            <img className=" h-16 " src="/svg/filter.svg" alt="filter" />
 
             <div className="invisible absolute  z-50 flex  flex-col bg-gray-100 py-1 px-4 text-gray-800 shadow-xl group-hover:visible">
               <a className="my-2 block border-b border-gray-100 py-1 font-semibold text-gray-500 hover:text-black md:mx-2">
-                Sunday
+                Електроника
               </a>
 
               <a className="my-2 block border-b border-gray-100 py-1 font-semibold text-gray-500 hover:text-black md:mx-2">
-                Monday
+                Спорт
               </a>
 
               <a className="my-2 block border-b border-gray-100 py-1 font-semibold text-gray-500 hover:text-black md:mx-2">
-                Tuesday
+                Дрехи
               </a>
 
               <a className="my-2 block border-b border-gray-100 py-1 font-semibold text-gray-500 hover:text-black md:mx-2">
-                Wednesday
+                Животни
               </a>
 
               <a className="my-2 block border-b border-gray-100 py-1 font-semibold text-gray-500 hover:text-black md:mx-2">
-                Thursday
+                Автомобили
               </a>
 
               <a className="my-2 block border-b border-gray-100 py-1 font-semibold text-gray-500 hover:text-black md:mx-2">
-                Friday
+                Работа
               </a>
 
               <a className="my-2 block border-b border-gray-100 py-1 font-semibold text-gray-500 hover:text-black md:mx-2">
-                Saturday
+                Недвижими имоти
               </a>
             </div>
           </div>
@@ -60,38 +87,36 @@ export default function Searchbare() {
 
         <div className="flex-none  m-0">
           <div className="group">
-            <img
-              className=" h-16"
-              src="/svg/location.svg"
-              alt="location"
-            />
+            <img className=" h-16" src="/svg/location.svg" alt="location" />
             <div className="invisible absolute  z-50 flex  flex-col bg-gray-100 py-1 px-4 text-gray-800 shadow-xl group-hover:visible">
+             
+            <input className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow" placeholder="Населено място..."/>
               <a className="my-2 block border-b border-gray-100 py-1 font-semibold text-gray-500 hover:text-black md:mx-2">
-                Sunday
+                Електроника
               </a>
 
               <a className="my-2 block border-b border-gray-100 py-1 font-semibold text-gray-500 hover:text-black md:mx-2">
-                Monday
+                Спорт
               </a>
 
               <a className="my-2 block border-b border-gray-100 py-1 font-semibold text-gray-500 hover:text-black md:mx-2">
-                Tuesday
+                Дрехи
               </a>
 
               <a className="my-2 block border-b border-gray-100 py-1 font-semibold text-gray-500 hover:text-black md:mx-2">
-                Wednesday
+                Животни
               </a>
 
               <a className="my-2 block border-b border-gray-100 py-1 font-semibold text-gray-500 hover:text-black md:mx-2">
-                Thursday
+                Автомобили
               </a>
 
               <a className="my-2 block border-b border-gray-100 py-1 font-semibold text-gray-500 hover:text-black md:mx-2">
-                Friday
+                Работа
               </a>
 
               <a className="my-2 block border-b border-gray-100 py-1 font-semibold text-gray-500 hover:text-black md:mx-2">
-                Saturday
+                Недвижими имоти
               </a>
             </div>
           </div>
