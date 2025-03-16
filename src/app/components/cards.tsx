ec1c0910a4f32836913637d28f1d480b22673056
Gogo200202 <gosheto420@gmail.com>
2025-03-16 17:55:40 +0200

    add cards to grid

--- src/app/components/cards.tsx
+++ src/app/components/cards.tsx
@@ -3,11 +3,53 @@ export default function Cards() {
     <div>
       <div className=" text-center text-5xl">Най-добри предложения</div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 ml-10 md:ml-40 lg:ml-96">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
+  
+<div className="grid h-auto grid-cols-3 lg:grid-cols-3 place-items-center gap-4">
+<div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+  <img className="w-full" src="/img/gothic-girl-4k-re-2048x2048-771658380.jpg" alt="Sunset in the mountains"/>
+  <div className="px-6 py-4">
+    <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+    <p className="text-gray-700 text-base">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+    </p>
+  </div>
+  <div className="px-6 pt-4 pb-2">
+    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+  </div>
+</div>
+<div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+  <img className="w-full" src="/img/gothic-girl-4k-re-2048x2048-771658380.jpg" alt="Sunset in the mountains"/>
+  <div className="px-6 py-4">
+    <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+    <p className="text-gray-700 text-base">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+    </p>
+  </div>
+  <div className="px-6 pt-4 pb-2">
+    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+  </div>
+</div>
+<div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+  <img className="w-full" src="/img/gothic-girl-4k-re-2048x2048-771658380.jpg" alt="Sunset in the mountains"/>
+  <div className="px-6 py-4">
+    <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+    <p className="text-gray-700 text-base">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+    </p>
+  </div>
+  <div className="px-6 pt-4 pb-2">
+    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+  </div>
+</div>
       </div>
-    </div>
+
+</div>
+      
   );
 }
