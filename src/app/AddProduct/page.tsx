138a5e1f012ed8140e414b8917c74910f665f94d
Gogo200202 <gosheto420@gmail.com>
2025-04-07 21:01:33 +0300

    add multipol img to add AddProduct

--- src/app/AddProduct/page.tsx
+++ src/app/AddProduct/page.tsx
@@ -1,20 +1,29 @@
 "use client";
 import { useState } from "react";
+
 export default function AddProduct() {
-  const [state, setState] = useState(
-    "https://static.vecteezy.com/system/resources/previews/016/916/479/non_2x/placeholder-icon-design-free-vector.jpg"
-  );
+  const [state, setState] = useState([
+    "https://static.vecteezy.com/system/resources/previews/016/916/479/non_2x/placeholder-icon-design-free-vector.jpg",
+    "https://static.vecteezy.com/system/resources/previews/016/916/479/non_2x/placeholder-icon-design-free-vector.jpg",
+    "https://static.vecteezy.com/system/resources/previews/016/916/479/non_2x/placeholder-icon-design-free-vector.jpg",
+    "https://static.vecteezy.com/system/resources/previews/016/916/479/non_2x/placeholder-icon-design-free-vector.jpg",
+    "https://static.vecteezy.com/system/resources/previews/016/916/479/non_2x/placeholder-icon-design-free-vector.jpg",
+    "https://static.vecteezy.com/system/resources/previews/016/916/479/non_2x/placeholder-icon-design-free-vector.jpg",
+  ]);
   var loadFile = function (event: any) {
     var input = event.target;
+ 
     var file = input.files[0];
     var type = file.type;
 
     //var output = document.getElementById('preview_img');
 
-    console.log(type);
-
     let obj = URL.createObjectURL(event.target.files[0]);
-    setState(obj);
+    let arr = state;
+    arr[event.target.id] = obj;
+
+    setState([...arr]);
+    console.log(state);
     // output.onload = function() {
     //     URL.revokeObjectURL(output.src) // free memory
     // }
@@ -70,22 +79,51 @@ export default function AddProduct() {
               />
             </div>
           </div>
-
-          <label>
-            <img
-              src={state}
-              alt="upload icon"
-              width="512"
-              height="512"
-            />
-            <div>
-              <h4>Upload a file</h4>
-           
-            </div>
-            <input type="file" id="doc"  onChange={loadFile} name="doc" hidden />
-
-          </label>
-         
+          <div className=" grid md:grid-cols-4 gap-2">
+            <label className="h-2/4 w-2/4">
+              <img src={state[0]} alt="upload icon" width="512" height="512" />
+              <div>
+                <h4>Upload a file</h4>
+              </div>
+              <input type="file" id="0" onChange={loadFile} hidden />
+            </label>
+            <label className="h-2/4 w-2/4">
+              <img src={state[1]} alt="upload icon" width="512" height="512" />
+              <div>
+                <h4>Upload a file</h4>
+              </div>
+              <input type="file" id="1" onChange={loadFile} hidden />
+            </label>
+            <label className="h-2/4 w-2/4">
+              <img src={state[2]} alt="upload icon" width="512" height="512" />
+              <div>
+                <h4>Upload a file</h4>
+              </div>
+              <input type="file" id="2" onChange={loadFile} hidden />
+            </label>
+            <label className="h-2/4 w-2/4">
+              <img src={state[3]} alt="upload icon" width="512" height="512" />
+              <div>
+                <h4>Upload a file</h4>
+              </div>
+              <input type="file" id="3" onChange={loadFile} hidden />
+            </label>
+            <label className="h-2/4 w-2/4">
+              <img src={state[4]} alt="upload icon" width="512" height="512" />
+              <div>
+                <h4>Upload a file</h4>
+              </div>
+              <input type="file" id="4" onChange={loadFile} hidden />
+            </label>
+            <label className="h-2/4 w-2/4">
+              <img src={state[5]} alt="upload icon" width="512" height="512" />
+              <div>
+                <h4>Upload a file</h4>
+              </div>
+              <input type="file" id="5" onChange={loadFile} hidden />
+            </label>
+          </div>
+          
 
           <div className="flex items-start mb-6">
             <div className="flex items-center h-5">
