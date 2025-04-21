a8fe0c57c13117d1dacb4a347ccd4ee8df5e8b5a
Gogo200202 <gosheto420@gmail.com>
2025-04-21 18:13:06 +0300

    replays def img

--- src/app/components/cards.tsx
+++ src/app/components/cards.tsx
@@ -6,7 +6,7 @@ export default function Cards() {
   
 <div className="grid h-auto grid-cols-1 lg:grid-cols-3 place-items-center gap-4">
 <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-  <img className="w-full" src="/img/gothic-girl-4k-re-2048x2048-771658380.jpg" alt="Sunset in the mountains"/>
+  <img className="w-full" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fuploads.vw-mms.de%2Fsystem%2Fproduction%2Fimages%2Fvwn%2F021%2F953%2Fimages%2Fc8f66396e4b8106041a94d6595053f3ad3c271d3%2FDB2016AU00844_web_1600.jpg%3F1649152300&f=1&nofb=1&ipt=a31e6b766e95ca6a21a75834d5d0d275adc3440c61af09a0ee1089cc42be29e3" alt="Sunset in the mountains"/>
   <div className="px-6 py-4">
     <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
     <p className="text-gray-700 text-base">
@@ -14,13 +14,13 @@ export default function Cards() {
     </p>
   </div>
   <div className="px-6 pt-4 pb-2">
-    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+    {/* <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
     <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span> */}
   </div>
 </div>
 <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-  <img className="w-full" src="/img/gothic-girl-4k-re-2048x2048-771658380.jpg" alt="Sunset in the mountains"/>
+  <img className="w-full" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpreview.free3d.com%2Fimg%2F2015%2F12%2F2279230813763536303%2Fkp10u1da.jpg&f=1&nofb=1&ipt=5bc77f3f08a188ef86b76834638b1e35ec0d16c8266f830c03958c8703a2393c" alt="Sunset in the mountains"/>
   <div className="px-6 py-4">
     <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
     <p className="text-gray-700 text-base">
@@ -28,13 +28,11 @@ export default function Cards() {
     </p>
   </div>
   <div className="px-6 pt-4 pb-2">
-    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+
   </div>
 </div>
 <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-  <img className="w-full" src="/img/gothic-girl-4k-re-2048x2048-771658380.jpg" alt="Sunset in the mountains"/>
+  <img className="w-full" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fae01.alicdn.com%2Fkf%2FHTB1ZhR2LXXXXXbnXFXXq6xXFXXXT%2F304-Stainless-Steel-Kitchen-Sink-faucet-kitchen-accessories-80-43-21cm-82-45-22cm.jpg&f=1&nofb=1&ipt=a19302786f459d2aaa12e03ce30e7ebfe7cea9d70805f43172278c9270b50840" alt="Sunset in the mountains"/>
   <div className="px-6 py-4">
     <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
     <p className="text-gray-700 text-base">
@@ -42,9 +40,7 @@ export default function Cards() {
     </p>
   </div>
   <div className="px-6 pt-4 pb-2">
-    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+    
   </div>
 </div>
       </div>
--- src/app/offers/[slug]/page.tsx
+++ src/app/offers/[slug]/page.tsx
@@ -17,7 +17,7 @@ export default function ExampleClientComponent() {
         <div className=" mt-10 ml-4 w-[350] lg:w-2/5 ">
           <img
             className=" rounded-2xl"
-            src="/img/gothic-girl-4k-re-2048x2048-771658380.jpg"
+            src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpreview.free3d.com%2Fimg%2F2015%2F12%2F2279230813763536303%2Fkp10u1da.jpg&f=1&nofb=1&ipt=5bc77f3f08a188ef86b76834638b1e35ec0d16c8266f830c03958c8703a2393c"
             alt="Sunset in the mountains"
           />
           <div className="text-black">tel:0899123843</div>
