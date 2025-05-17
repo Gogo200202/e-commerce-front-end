e77b170fc1d06062a2cf0b13cda5a5f0dcc9849d
Gogo200202 <gosheto420@gmail.com>
2025-05-17 20:46:37 +0300

    add item update with back end

--- src/app/components/cards.tsx
+++ src/app/components/cards.tsx
@@ -1,51 +1,44 @@
-export default function Cards() {
+export default async function Cards() {
+  interface item {
+    _id: number;
+    img: string;
+    name: string;
+    description: string;
+  }
+
+  const data = await fetch("http://localhost:8080/getAllItems");
+  const items: any = await data.json();
+
   return (
     <div>
       <div className=" text-center text-5xl">Най-добри предложения</div>
 
-  
-<div className="grid h-auto grid-cols-1 lg:grid-cols-3 place-items-center gap-4">
-<div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-  <img className="w-full" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fuploads.vw-mms.de%2Fsystem%2Fproduction%2Fimages%2Fvwn%2F021%2F953%2Fimages%2Fc8f66396e4b8106041a94d6595053f3ad3c271d3%2FDB2016AU00844_web_1600.jpg%3F1649152300&f=1&nofb=1&ipt=a31e6b766e95ca6a21a75834d5d0d275adc3440c61af09a0ee1089cc42be29e3" alt="Sunset in the mountains"/>
-  <div className="px-6 py-4">
-    <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-    <p className="text-gray-700 text-base">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-    </p>
-  </div>
-  <div className="px-6 pt-4 pb-2">
-    {/* <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span> */}
-  </div>
-</div>
-<div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-  <img className="w-full" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpreview.free3d.com%2Fimg%2F2015%2F12%2F2279230813763536303%2Fkp10u1da.jpg&f=1&nofb=1&ipt=5bc77f3f08a188ef86b76834638b1e35ec0d16c8266f830c03958c8703a2393c" alt="Sunset in the mountains"/>
-  <div className="px-6 py-4">
-    <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-    <p className="text-gray-700 text-base">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-    </p>
-  </div>
-  <div className="px-6 pt-4 pb-2">
-
-  </div>
-</div>
-<div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-  <img className="w-full" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fae01.alicdn.com%2Fkf%2FHTB1ZhR2LXXXXXbnXFXXq6xXFXXXT%2F304-Stainless-Steel-Kitchen-Sink-faucet-kitchen-accessories-80-43-21cm-82-45-22cm.jpg&f=1&nofb=1&ipt=a19302786f459d2aaa12e03ce30e7ebfe7cea9d70805f43172278c9270b50840" alt="Sunset in the mountains"/>
-  <div className="px-6 py-4">
-    <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-    <p className="text-gray-700 text-base">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-    </p>
-  </div>
-  <div className="px-6 pt-4 pb-2">
-    
-  </div>
-</div>
+      <div className="grid h-auto grid-cols-1 lg:grid-cols-3 place-items-center gap-4">
+        {items.Items.map((post: item) => (
+          <div key={post._id} className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+            <img className="w-full" src={post.img} alt="Sunset in the mountains" />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2 text-gray-500">
+                {post.name}
+              </div>
+              <p className="text-gray-700 text-base">
+                {post.description}
+              </p>
+            </div>
+            <div className="px-6 pt-4 pb-2">
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #photography
+              </span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #travel
+              </span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #winter
+              </span>
+            </div>
+          </div>
+        ))}
       </div>
-
-</div>
-      
+    </div>
   );
 }
--- src/app/offers/[slug]/page.tsx
+++ src/app/offers/[slug]/page.tsx
@@ -1,39 +1,62 @@
-"use client";
+'use server'
 
-import { useParams } from "next/navigation";
-import Searchbare from "@/app/components/searchbare";
 
-export default function Offers() {
-  const params = useParams<{ slug: string }>();
 
+
+export default async function Offers( {params}: {params:{slug:string}} ) {
+
+  const  slug  = await params;
+let id :string= await slug.slug;
+console.log(  id );
+
+
+
+  let img;
+  let name;
+  let description;
+  try {
+
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ _id: id })
+    };
+
+    const response = await fetch(
+      "http://localhost:8080/getById"
+    ,requestOptions);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    let data = await response.json();
+    data = data.item;
+   img = data.img;
+    name = data.name;
+    description = data.description;
+  } catch (error: any) {
+    console.error(error.message);
+  }
 
   return (
     <>
-    <Searchbare/>
-    <div className="grid h-screen place-items-center">
-
-      <div className="h-auto w-[380px] lg:h-2/3 lg:w-2/3 bg-amber-100  rounded-2xl lg:relative ">
-        <div className=" mt-10 ml-4 w-[350] lg:w-2/5 ">
-          <img
-            className=" rounded-2xl"
-            src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpreview.free3d.com%2Fimg%2F2015%2F12%2F2279230813763536303%2Fkp10u1da.jpg&f=1&nofb=1&ipt=5bc77f3f08a188ef86b76834638b1e35ec0d16c8266f830c03958c8703a2393c"
-            alt="Sunset in the mountains"
-          />
-          <div className="text-black">tel:0899123843</div>
-        </div>
-        <div className="lg:absolute top-1/6 right-0 lg:w-3/6">
-          <div className=" m-2 text-black  font-bold  ">
-            Testvane na zaglavieto
+      <div className="grid h-screen place-items-center">
+        <div className="h-auto w-[380px] lg:h-2/3 lg:w-2/3 bg-amber-100  rounded-2xl lg:relative ">
+          <div className=" mt-10 ml-4 w-[350] lg:w-2/5 ">
+            <img
+              className=" rounded-2xl"
+              src={img}
+              alt="Sunset in the mountains"
+            />
+            <div className="text-black">tel:0899123843</div>
           </div>
-          <div className="text-black font-bold m-2">Take goriq neshto</div>
-          <div className="m-2 text-black">
-            test da produkti s mnogo dulgi imena test da produkti s mnogo dulgi
-            imena test da produkti s mnogo dulgi imena test da produkti s mnogo
-            dulgi imena
+          <div className="lg:absolute top-1/6 right-0 lg:w-3/6">
+            <div className=" m-2 text-black  font-bold  ">{name}</div>
+            <div className="text-black font-bold m-2">Take goriq neshto</div>
+            <div className="m-2 text-black">{description}</div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
