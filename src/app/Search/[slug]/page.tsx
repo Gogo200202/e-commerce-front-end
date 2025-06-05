c6446910fbcec7ad1321969e56c0d9ded56d72a9
Gogo200202 <gosheto420@gmail.com>
2025-06-05 17:03:14 +0300

    fix top bare

--- src/app/Search/[slug]/page.tsx
+++ src/app/Search/[slug]/page.tsx
@@ -47,7 +47,7 @@ export default function Search() {
 
   return (
     <>
-    {console.log(data)}
+   
     <Topbare/>
       <Searchbare />
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
--- src/app/components/topbare.tsx
+++ src/app/components/topbare.tsx
@@ -1,8 +1,21 @@
+"use client";
 import Link from "next/link";
 import { checkIfLogIn, getName } from "../_actions/cookie";
-export default async function Topbare() {
-  let logedIN = await checkIfLogIn();
-  let name=await getName();
+import { useState, useEffect } from "react";
+
+export default function Topbare() {
+  const [logedIN, setLogedIN] = useState(false);
+  const [name, setName] = useState("");
+  useEffect(() => {
+    async function GetDataFromCookie() {
+      let logedIN = await checkIfLogIn();
+
+      let name = await getName();
+      setLogedIN(logedIN);
+      setName(name?.value!);
+    }
+    GetDataFromCookie();
+  }, [logedIN, name]);
 
   return (
     <div className="h-24  bg-topbare relative">
@@ -12,9 +25,12 @@ export default async function Topbare() {
           src="/svg/ecommerce.svg"
         />
       </Link>
-      
+
       <div className="absolute top-5  right-5 grid grid-cols-2 grid-rows-2 ">
-        <div className="h-10 w-40 border-t-8 border-(--color-topbare) hover:text-gray-500"> {name?.value}</div>
+        <div className="h-10 w-40 border-t-8 border-(--color-topbare) hover:text-gray-500">
+          {" "}
+          {name?.value}
+        </div>
         {logedIN ? (
           <Link
             className="h-10 w-40 border-t-8 border-(--color-whiteSearchBar)  rounded-md bg-whiteSearchBar   text-black text-center  hover:text-white "
@@ -30,7 +46,6 @@ export default async function Topbare() {
             Създаите профил
           </Link>
         )}
-        
       </div>
     </div>
   );
