eb8a3d9bac56ceaa282243d34047c4a517bba304
Gogo200202 <gosheto420@gmail.com>
2025-07-07 15:57:25 +0300

    add category and location to search bar again

--- src/app/components/searchbare.tsx
+++ src/app/components/searchbare.tsx
@@ -3,7 +3,6 @@ import { useRouter } from "next/navigation";
 import React, { useState, useEffect } from "react";
 import { useParams } from "next/navigation";
 
-
 import { usePathname } from "next/navigation";
 
 function isEmpty(obj: any) {
@@ -15,24 +14,78 @@ function isEmpty(obj: any) {
 
   return true;
 }
-
-
+//  <option value="Електроника">Електроника</option>
+//                   <option value="Недвижими имоти">Недвижими имоти</option>
+//                   <option value="Дрехи">Дрехи</option>
+//                   <option value="Автомобили">Автомобили</option>
+//                   <option value="Животни">Животни</option>
+//                   <option value="За дома">За дома</option>
+//                   <option value="Работа">Работа</option>
+const categories = [
+  {
+    name: "Електроника",
+    id: 0,
+  },
+  {
+    name: "Недвижими имоти",
+    id: 1,
+  },
+  {
+    name: "Дрехи",
+    id: 2,
+  },
+  {
+    name: "Автомобили",
+    id: 3,
+  },
+  {
+    name: "Животни",
+    id: 4,
+  },
+  ,
+  {
+    name: "За дома",
+    id: 5,
+  },
+  {
+    name: "Работа",
+    id: 6,
+  },
+];
 
 let TakeOneThing = true;
 export default function searchable() {
   const router = useRouter();
 
+  const [input, setInput] = useState("");
+  const [category, setCategory] = useState("");
+  const [location, setLocation] = useState("");
+
   function pushRoutToAll() {
-    router.push("/Search/" + input );
+    router.push("/Search/" + input+"/"+category+"/"+location);
   }
 
-  const [input, setInput] = useState("");
+  function AddCategory(event: any) {
+    let className =
+      "my-2 block border-b border-gray-100 py-1 font-semibold text-gray-500 hover:text-black md:mx-2";
+
+    for (let i = 0; i < event.target.parentElement.children.length; i++) {
+      event.target.parentElement.children[i].className = className;
+    }
+    let text = event.target.text;
+
+    event.target.className = "font-bold";
+    setCategory(event.target.text);
+  }
+
+  function typingLocation(event: any) {
+    setLocation(event.target.value);
+  }
 
   function typing(event: any) {
     setInput(event.target.value);
   }
 
- 
   function searchbareIcon() {
     TakeOneThing = true;
 
@@ -72,7 +125,7 @@ export default function searchable() {
       <div className="bg-whiteSearchBar h-full w-full rounded-md flex">
         <img
           onClick={searchbareIcon}
-          className="h-16 ml-1  "
+          className="h-16 ml-1  flex-none"
           src="/svg/search.svg"
           alt="search"
         />
@@ -80,11 +133,43 @@ export default function searchable() {
           type="text"
           onChange={typing}
           defaultValue={input}
-          className="h-16   text-black focus:outline-none "
+          className="h-16 w-1/3 md:w-2/3 lg:w-5/6 flex-none text-black focus:outline-none "
           placeholder="Search branch name..."
           required
         />
 
+        <div className="flex-none">
+          <div className="group">
+            <img className=" h-16 " src="/svg/filter.svg" alt="filter" />
+
+            <div className="invisible absolute  z-50 flex  flex-col bg-gray-100 py-1 px-4 text-gray-800 shadow-xl group-hover:visible">
+              {categories.map(function (data) {
+                return (
+                  <a
+                    onClick={AddCategory}
+                    key={data?.id}
+                    className="my-2 block border-b border-gray-100 py-1 font-semibold text-gray-500 hover:text-black md:mx-2"
+                  >
+                    {data?.name}
+                  </a>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+
+        <div className="flex-none  m-0">
+          <div className="group">
+            <img className=" h-16" src="/svg/location.svg" alt="location" />
+            <div className="invisible absolute  z-50 flex  flex-col bg-gray-100 py-1 px-4 text-gray-800 shadow-xl group-hover:visible">
+              <input
+                onChange={typingLocation}
+                className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+                placeholder="Населено място..."
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
